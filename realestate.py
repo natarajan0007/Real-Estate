f881262da62e099bec755aeb3c60c7507fb1b593
magilla3012 <74724920+magilla3012@users.noreply.github.com>
2020-11-19 21:13:02 +0530

    configuration and views files are added to repo

--- realestate.py
+++ realestate.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail,Message
 
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
